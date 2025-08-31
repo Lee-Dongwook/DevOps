00d2e3c874c800ddbafaf1aa1d0de4526514b8f2
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-08-31 18:43:05 +0900

    feat: restart

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  common_tags = {
-    company    = "leetech"
-    owner      = "leetech DevOps Team"
-    team-email = "team-devops@leetech.com"
-    time       = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
-  }
-
-}
